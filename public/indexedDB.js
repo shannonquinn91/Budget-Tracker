d37bf308e17249e2328b8b2b4d7d9a7c3b41d6e1
Shannon Quinn <shannon.quinn91@gmail.com>
2021-02-12 15:25:09 -0500

    Added indexeddb.js, webmanifest, and html routes

---
+++ public/indexedDB.js
@@ -0,0 +1,72 @@
+//event listener
+window.addEventListener("online", checkIndexedDB);
+
+//initialize db
+let db;
+
+//new indexeddb request for "budget", version 1
+const request = indexedDB.open("budget", 1);
+
+//create pending object store for transactions made offline, auto-increment
+request.onupgradeneeded = function(e) {
+    const db = e.target.result;
+
+    //check if online before reading from db
+    if (navigator.online) {
+        checkIndexedDB();
+    }
+};
+
+//error handling
+request.onerror = function(e) {
+    console.log("Error! " + e.target.errorCode);
+};
+
+function saveRecord(record) {
+    //transaction on pending db with readwrite permissions
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    //access pending object store
+    const store = transaction.objectStore("pending");
+
+    //add record to pending object store
+    store.add(record);
+};
+
+function checkIndexedDB() {
+    //open transaction
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    //access pending object store
+    const store = transaction.objectStore("pending");
+
+    //get records from the store; store in a variable
+    const getAll = store.getAll();
+
+    getAll.onsuccess = function() {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*", 
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                //if success, open transaction on pending 
+                const transaction = db.transaction(["pending"], "readwrite");
+
+                //access pending object store
+                const store = transaction.objectStore("pending");
+
+                //clear the pending object store
+                store.clear();
+            })
+        }
+    }
+}
+
+//check for app coming back online
+window.addEventListener("online", checkIndexedDB);
--- server.js
+++ server.js
@@ -17,12 +17,25 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useFindAndModify: false,
+  useCreateIndex: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
+require('./routes/html-routes')(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
+
+{/* <script type="text/javascript">
+    (function () {
+      if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("./service-worker.js", { scope: "/" })
+          .then(() => console.log("Service Worker registered successfully."))
+          .catch(error => console.log("Service Worker registration failed:", error));
+      }
+    })();
+  </script> */}
