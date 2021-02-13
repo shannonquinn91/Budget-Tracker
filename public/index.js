58b6172583332ece5d70be558c7d5a2985f25ca1
Shannon Quinn <shannon.quinn91@gmail.com>
2021-02-13 18:19:10 -0500

    deploying

--- public/index.js
+++ public/index.js
@@ -1,8 +1,4 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("./service-worker.js")
-  .then(() => console.log("Service Worker registered successfully."))
-  .catch(error => console.log("Service Worker registration failed:", error));
-}
+
 
 let transactions = [];
 let myChart;
